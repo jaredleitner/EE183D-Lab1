0f45580ded5fdb732b3485ee22b19acc36e6239b
jaredleitner <35789289+jaredleitner@users.noreply.github.com>
2018-01-25 12:31:06 -0800

    Delete OperationalSpace.m

--- OperationalSpace.m
+++
@@ -1,3 +0,0 @@
-function OpSpace=OperationalSpace(theta)
-    OpSpace=rotationZ(theta(1))*translationX(50)*rotationZ(theta(2))*translationX(30)*translationZ(15)*rotationZ(theta(3))*rotationX(pi/2)*rotationZ(theta(4));
-end
