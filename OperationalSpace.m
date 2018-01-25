db858f41a760a2706d3bdd38550a231dc1c657e1
jaredleitner <35789289+jaredleitner@users.noreply.github.com>
2018-01-25 12:18:09 -0800

    Add files via upload

---
+++ OperationalSpace.m
@@ -0,0 +1,3 @@
+function OpSpace=OperationalSpace(theta)
+    OpSpace=rotationZ(theta(1))*translationX(50)*rotationZ(theta(2))*translationX(30)*translationZ(15)*rotationZ(theta(3))*rotationX(pi/2)*rotationZ(theta(4));
+end
---
+++ translationZ.m
@@ -0,0 +1,3 @@
+function transZ=translationZ(d)
+    transZ=[1 0 0 0; 0 1 0 0; 0 0 1 d; 0 0 0 1];
+end
